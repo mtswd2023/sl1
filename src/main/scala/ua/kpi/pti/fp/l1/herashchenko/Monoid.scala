a3e6d7a4c27ea8e45aab371b13b045600712a83f
heragero <volger-ipt25@lll.kpi.ua>
2023-06-05 13:48:56 +0300

    deleted old methods from Monoid; added apply function to Monoid

--- src/main/scala/ua/kpi/pti/fp/l1/herashchenko/Monoid.scala
+++ src/main/scala/ua/kpi/pti/fp/l1/herashchenko/Monoid.scala
@@ -8,30 +8,30 @@ trait Monoid[A] {
     ): A
     def empty: A
 }
-object Monoid {
-
-    implicit def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]]{
-        override def empty: List[A] = List.empty[A]
-        override def combine(x: List[A], y: List[A]): List[A] = x ++ y
-    }
-    implicit def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]]{
-        override def empty: Option[A] = None
-        override def combine(x: Option[A], y: Option[A]): Option[A] = x.orElse(y)
-    }
-    implicit def setMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]]{
-        override def empty: Set[A] = Set.empty[A]
-        override def combine(x: Set[A], y: Set[A]): Set[A] = x ++ y
-    }
-}
-
-
 trait Empty[F[_]]{
-    def get[A]: F[A]
-}
+        def get[A]: F[A]
+    }
 trait Combine[F[_]]{
     def combine[A](x:F[A], y:F[A]): F[A]
+    }
+
+
+
+object Monoid {
+
+    def apply[A,F[_]](implicit monoid: Monoid[F[A]]): Monoid[F[A]] = monoid
+    
+    implicit def genericMonoid[A,F[_]](implicit empt: Empty[F], comb: Combine[F]) = new Monoid[F[A]]{
+        override def combine(x: F[A], y: F[A]): F[A] = comb.combine(x,y)
+        override def empty: F[A] = empt.get
+    }
 }
-object Empty{   
+
+
+object Empty{
+    def apply[F[_]](implicit instance: Empty[F]): Empty[F] = instance
+
+    
     implicit val listEmpty: Empty[List] = new Empty[List]{
         def get[A]: List[A] = List.empty[A]
     }
@@ -43,13 +43,17 @@ object Empty{
     }
 }
 object Combine{
-    implicit val listCombine: Combine[List] = new Combine[List]{
+        def apply[F[_]](implicit instance: Combine[F]): Combine[F] = instance
+
+
+        implicit val listCombine: Combine[List] = new Combine[List]{
         def combine[A](x: List[A], y: List[A]): List[A] = x ++ y
-    }
-    implicit val optionCombine: Combine[Option] = new Combine[Option]{
+        }
+        implicit val optionCombine: Combine[Option] = new Combine[Option]{
         def combine[A](x: Option[A], y: Option[A]): Option[A] = x orElse y
-    }
-    implicit val setCombine: Combine[Set] = new Combine[Set]{
+         }
+        implicit val setCombine: Combine[Set] = new Combine[Set]{
         def combine[A](x: Set[A], y: Set[A]): Set[A] = x ++ y
-    }
+        }
 }
+
