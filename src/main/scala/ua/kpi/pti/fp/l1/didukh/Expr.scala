1ee068e4f52eb62abfb87475ec2df57638f937cb
prumat4 <didukhmaksym4@gmail.com>
2023-06-07 00:13:01 +0300

    implement case classes Var, Num, Bool

--- src/main/scala/ua/kpi/pti/fp/l1/didukh/Expr.scala
+++ src/main/scala/ua/kpi/pti/fp/l1/didukh/Expr.scala
@@ -1,6 +1,6 @@
 package ua.kpi.pti.fp.l1.didukh
 
-sealed trait Expr {
+trait Expr {
   // Var(name) | Num(int) | Bool(boolean) | Add(expr, expr) | And(expr, expr) | Cond(bool, expr, expr
   // Some(error) if we have e.g. Add(Bool(true), Num(10)), None otherwise
   def typecheck(): Option[String]
@@ -9,4 +9,29 @@ sealed trait Expr {
   // note that this is incomplete - please refine type hierarchy in a way that could
   // prevent e.g. having Add(Bool,Num) or And(Num,Num). Maybe we can have different sets of types
   // for results of parsing and actual Expr?
+}
+
+case class Var(name: String) extends Expr {
+  def typecheck(): Option[String] = None
+
+  def eval(vars: Map[String, Either[Int, Boolean]]): Either[String, Either[Int, Boolean]] = {
+    vars.get(name) match {
+      case Some(value) => Right(value)
+      case None => Left(s"Variable $name is undefined")
+    }
+  }
+}
+
+case class Num(value: Int) extends Expr {
+  def typecheck(): Option[String] = None 
+
+  def eval(vars: Map[String, Either[Int, Boolean]]): Either[String, Either[Int, Boolean]] =
+    Right(Left(value)) 
+}
+
+case class Bool(value: Boolean) extends Expr {
+  def typecheck(): Option[String] = None 
+
+  def eval(vars: Map[String, Either[Int, Boolean]]): Either[String, Either[Int, Boolean]] =
+    Right(Right(value)) 
 }
--- src/test/scala/ua/kpi/pti/fp/l1/assignment/didukh/ExprTests.scala
+++ src/test/scala/ua/kpi/pti/fp/l1/assignment/didukh/ExprTests.scala
@@ -3,13 +3,34 @@ package ua.kpi.pti.fp.l1.assignment.didukh
 import ua.kpi.pti.fp.l1.assignment.Assignment 
 import ua.kpi.pti.fp.l1.assignment.L1PropOrTest
 import ua.kpi.pti.fp.l1.assignment.L1PropOrTest.L1SimpleTest
-// import ua.kpi.pti.fp.l1.didukh.Expr
+import ua.kpi.pti.fp.l1.didukh.{ Var, Num, Bool }
+// import ua.kpi.pti.fp.l1.didukh.Add
+// import ua.kpi.pti.fp.l1.didukh.Cond
+// import ua.kpi.pti.fp.l1.didukh.And
+
+
+// import ua.kpi.pti.fp.l1.didukh.ExprParse
 
 object ExprTests extends Assignment {
   override def assigneeFullName: String = "Дідух Максим Андрійович"
 
-  override def props: List[(String, L1PropOrTest)] = List(
-    "1 + 1 == 2" -> L1SimpleTest.of(assert(1+1 == 2))
-  )
+  val vars = Map("x" -> Left(10), "y" -> Right(true))
 
+  override def props: List[(String, L1PropOrTest)] = List(
+    "Var test" -> L1SimpleTest.of {
+      val expr = Var("x")
+      val result = expr.eval(vars)
+      assertEquals(result, Right(Left(10)))
+    },
+    "Num test" -> L1SimpleTest.of {
+      val expr = Num(42)
+      val result = expr.eval(vars)
+      assertEquals(result, Right(Left(42)))
+    },
+    "Bool test" -> L1SimpleTest.of {
+      val expr = Bool(true)
+      val result = expr.eval(vars)
+      assertEquals(result, Right(Right(true)))
+    }
+  )
 }
