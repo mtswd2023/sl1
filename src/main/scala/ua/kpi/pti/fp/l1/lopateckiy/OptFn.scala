5e08ede89597478c44e21d6d7a1f813c260a6567
anabolic23 <mishalopateckiy@gmail.com>
2023-06-06 19:06:27 +0300

    added OptFn and implemented methods

---
+++ src/main/scala/ua/kpi/pti/fp/l1/lopateckiy/OptFn.scala
@@ -0,0 +1,17 @@
+package ua.kpi.pti.fp.l1.lopateckiy
+
+//Option
+case class OptFn[A, B](f: A => Option[B]) {
+  def map[C](fn: B => C): OptFn[A, C] = OptFn(a => f(a).map(fn))
+
+  def flatMap[C](fn: B => OptFn[A, C]): OptFn[A, C] = OptFn((a: A) => f(a) match {
+    case Some(b) => fn(b).f(a)
+    case None => None
+  })
+
+  def andThen[C](g: B => OptFn[B, C]): OptFn[A, C] = OptFn((a: A) => f(a) match {
+    case Some(b) => g(b).f(b)
+    case None => None
+  })
+}
+
