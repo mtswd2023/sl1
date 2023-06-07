f802d010a2f62b3920a04cccee9afa98923e6e9f
prumat4 <didukhmaksym4@gmail.com>
2023-06-07 23:05:01 +0300

    implement Add, And, Cond classes and some tests

--- src/main/scala/ua/kpi/pti/fp/l1/didukh/Expr.scala
+++ src/main/scala/ua/kpi/pti/fp/l1/didukh/Expr.scala
@@ -35,3 +35,58 @@ case class Bool(value: Boolean) extends Expr {
   def eval(vars: Map[String, Either[Int, Boolean]]): Either[String, Either[Int, Boolean]] =
     Right(Right(value)) 
 }
+
+case class Add(expr1: Expr, expr2: Expr) extends Expr {
+  def typecheck(): Option[String] = {
+    (expr1.typecheck(), expr2.typecheck()) match {
+      case (None, None) => None
+      case (Some(error), None) => Some(error)
+      case (None, Some(error)) => Some(error)
+      case (Some(error1), Some(error2)) => Some(s"Type error: $error1 and $error2")
+    }
+  }
+
+  def eval(vars: Map[String, Either[Int, Boolean]]): Either[String, Either[Int, Boolean]] = {
+    (expr1.eval(vars), expr2.eval(vars)) match {
+      case (Right(Left(value1)), Right(Left(value2))) => Right(Left(value1 + value2))
+      case _ => Left("Type error: cannot perform addition of non-integer values")
+    }
+  }
+}
+
+case class And(expr1: Expr, expr2: Expr) extends Expr {
+  def typecheck(): Option[String] = {
+    (expr1.typecheck(), expr2.typecheck()) match {
+      case (None, None) => None
+      case (Some(error), None) => Some(error)
+      case (None, Some(error)) => Some(error)
+      case (Some(error1), Some(error2)) => Some(s"Type error: $error1 and $error2")
+    }
+  }
+
+  def eval(vars: Map[String, Either[Int, Boolean]]): Either[String, Either[Int, Boolean]] = {
+    (expr1.eval(vars), expr2.eval(vars)) match {
+      case (Right(Right(value1)), Right(Right(value2))) => Right(Right(value1 && value2))
+      case _ => Left("Type error: cannot perform logical AND on non-boolean values")
+    }
+  }
+}
+
+case class Cond(bool: Expr, expr1: Expr, expr2: Expr) extends Expr {
+  def typecheck(): Option[String] = {
+    (bool.typecheck(), expr1.typecheck(), expr2.typecheck()) match {
+      case (None, None, None) => None
+      case (Some(error), _, _) => Some(error)
+      case (_, Some(error), _) => Some(error)
+      case (_, _, Some(error)) => Some(error)
+    }
+  }
+
+  def eval(vars: Map[String, Either[Int, Boolean]]): Either[String, Either[Int, Boolean]] = {
+    bool.eval(vars) match {
+      case Right(Right(true)) => expr1.eval(vars)
+      case Right(Right(false)) => expr2.eval(vars)
+      case _ => Left("Type error: condition is not a boolean value")
+    }
+  }
+}
--- src/test/scala/ua/kpi/pti/fp/l1/assignment/didukh/ExprTests.scala
+++ src/test/scala/ua/kpi/pti/fp/l1/assignment/didukh/ExprTests.scala
@@ -3,13 +3,9 @@ package ua.kpi.pti.fp.l1.assignment.didukh
 import ua.kpi.pti.fp.l1.assignment.Assignment 
 import ua.kpi.pti.fp.l1.assignment.L1PropOrTest
 import ua.kpi.pti.fp.l1.assignment.L1PropOrTest.L1SimpleTest
-import ua.kpi.pti.fp.l1.didukh.{ Var, Num, Bool }
-// import ua.kpi.pti.fp.l1.didukh.Add
-// import ua.kpi.pti.fp.l1.didukh.Cond
-// import ua.kpi.pti.fp.l1.didukh.And
+import ua.kpi.pti.fp.l1.didukh.{ Var, Num, Bool, Add, And, Cond }
 
-
-// import ua.kpi.pti.fp.l1.didukh.ExprParse
+// import ua.kpi.pti.fp.l1.didukh.ExprParse // точно так це треба робтии?
 
 object ExprTests extends Assignment {
   override def assigneeFullName: String = "Дідух Максим Андрійович"
@@ -31,6 +27,41 @@ object ExprTests extends Assignment {
       val expr = Bool(true)
       val result = expr.eval(vars)
       assertEquals(result, Right(Right(true)))
+    },
+    "Add test" -> L1SimpleTest.of {
+      val expr = Add(Num(10), Num(20))
+      val result = expr.eval(vars)
+      assertEquals(result, Right(Left(30)))
+    },
+    "Add with non-integer values test" -> L1SimpleTest.of {
+      val expr = Add(Num(10), Bool(true))
+      val result = expr.eval(vars)
+      assertEquals(result, Left("Type error: cannot perform addition of non-integer values"))
+    },
+    "And test" -> L1SimpleTest.of {
+      val expr = And(Bool(true), Bool(false))
+      val result = expr.eval(vars)
+      assertEquals(result, Right(Right(false)))
+    },
+    "And with non-boolean values test" -> L1SimpleTest.of {
+      val expr = And(Bool(true), Num(10))
+      val result = expr.eval(vars)
+      assertEquals(result, Left("Type error: cannot perform logical AND on non-boolean values"))
+    },
+    "Cond true test" -> L1SimpleTest.of {
+      val expr = Cond(Bool(true), Num(10), Num(20))
+      val result = expr.eval(vars)
+      assertEquals(result, Right(Left(10)))
+    },
+    "Cond false test" -> L1SimpleTest.of {
+      val expr = Cond(Bool(false), Num(10), Num(20))
+      val result = expr.eval(vars)
+      assertEquals(result, Right(Left(20)))
+    },
+    "Cond with non-boolean condition test" -> L1SimpleTest.of {
+      val expr = Cond(Num(10), Num(10), Num(20))
+      val result = expr.eval(vars)
+      assertEquals(result, Left("Type error: condition is not a boolean value"))
     }
   )
 }
